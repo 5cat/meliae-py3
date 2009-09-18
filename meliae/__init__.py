5287c49f221234e769fdeb7f543d7b935451c71c
John Arbash Meinel <john@arbash-meinel.com>
2009-09-18 12:00:34 -0500

    Change license to GPL v3.
    Official policy is to use GPLv3 not v3 or later, and not LGPL.

--- meliae/__init__.py
+++ meliae/__init__.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 # Copyright (C) 2009 Canonical Ltd
 # 
 # This program is free software: you can redistribute it and/or modify
@@ -13,20 +12,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-"""Run the Meliae test suite."""
+"""A simple way to dump memory consumption of a running python program."""
 
-import sys
+version_info = (0, 0, 1, 'dev', 0)
+__version__ = '.'.join(map(str, version_info))
 
-
-def main(args):
-    import optparse
-    p = optparse.OptionParser()
-    p.add_option('--verbose', '-v', action='store_true',
-                 help='run verbose tests')
-
-    (opts, args) = p.parse_args(args)
-    import meliae.tests
-    return not meliae.tests.run_suite(verbose=opts.verbose)
-
-if __name__ == '__main__':
-    sys.exit(main(sys.argv[1:]))
